728b6ebd11b54deed20f793bff6dc0d6c2b64a0e
Robert Vitoriano <robertvitorianodev@gmail.com>
2020-03-03 20:25:43 -0300

    Unnercessary console.log call removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!');
-
 const weatherForm = document.querySelector('form');
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
